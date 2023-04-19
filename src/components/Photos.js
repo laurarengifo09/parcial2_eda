4249ba58daf061fd82b790b533fa45239dfe1ae1
laurarengifo09 <68392532+laurarengifo09@users.noreply.github.com>
2023-04-19 15:15:49 -0500

    parcial_2

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,33 +1,49 @@
-import React, { useEffect } from 'react';
-import {  useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchPhotos } from '../actions/action';
+
 
 export const Photo = () => {
   const dispatch = useDispatch();
   const photos = useSelector((state) => state.photos.photos);
   const loading = useSelector((state) => state.photos.loading);
+  const error = useSelector((state) => state.photos.error);
+  const [fetching, setFetching] = useState(false);
 
   useEffect(() => {
-    dispatch(fetchPhotos());
-  }, [dispatch]);
+    if (fetching) {
+      dispatch(fetchPhotos());
+      setFetching(false);
+    }
+  }, [dispatch, fetching]);
+
+  const handleButtonClick = () => {
+    setFetching(true);
+  };
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div>
-      <h1> ¡Photos!</h1>
-      <div className='lista-fotos'>
-        {photos.map((photo)=>(
-            <div key={photo.id} className='photo-id'>
-                <img src={photo.thumbnailUrl} alt={photo.title}/>
-                <div className='titulo'> {photo.title}</div>
-                </div>
+      <h1>Photos</h1>
+      <button onClick={handleButtonClick}>Fetch photos</button>
+      <div className="photos-list">
+        {photos.map((photo) => (
+          <div key={photo.id} className="photo-item">
+            <img src={photo.thumbnailUrl} alt={photo.title} />
+            <div className="photo-title">{photo.title}</div>
+          </div>
         ))}
-        </div>
       </div>
+    </div>
   );
-}
+};
+
 
 
